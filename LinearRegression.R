8d8c5a3632f0cea12c21997d1f6d565f4286a3cd
ConradKilroy <conradkilroy@gmail.com>
2016-09-11 16:01:03 -0700

    Update LinearRegression.R

--- LinearRegression.R
+++ LinearRegression.R
@@ -6,7 +6,7 @@
 
 # Import the data from the data file *CarsData.csv* and store it in `cars`. The data give the speed of cars and the distances taken to stop. Note that the data were recorded in the 1920s.
 
-cars <- read.csv("CarsData.csv")
+cars <- read.csv("LinearRegressionModelCars.csv")
 head(cars)
 
 # To compute the linear regression we use the function `lm()` and store the resulting data in `cars.lr`. In this case `dist`, the dependent variable, is modelled by the independent variable `speed`.
